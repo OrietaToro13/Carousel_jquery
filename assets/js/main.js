7be1849d30ea5c3b243ab79614a33395b15855c3
Orieta Toro <orieta.toro@gmail.com>
2017-07-04 21:49:12 -0400

    slider pendiente

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    slideWidth: 300,
+    minSlides: 2,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 10
+  });
+});
+//lo vi en una pagina y no se si me funcione
