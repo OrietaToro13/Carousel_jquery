f8ffe7e0680640d4531c17e563ad067165e50597
Orieta Toro <orieta.toro@gmail.com>
2017-07-07 18:51:10 -0400

    no me funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
-$(document).ready(function(){
-  $('.slider').bxSlider({
-    slideWidth: 300,
-    minSlides: 2,
-    maxSlides: 3,
-    moveSlides: 1,
-    slideMargin: 10
-  });
-});
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+        
 //lo vi en una pagina y no se si me funcione
